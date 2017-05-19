cb057fb44bed614cacb547af95a6feb726360bde
Agustina <aguusnm@gmail.com>
2017-05-18 22:33:56 -0300

    agrego funciones al form de ventas

--- PAV1_TPI/FormVentas.vb
+++ PAV1_TPI/FormVentas.vb
@@ -1,4 +1,6 @@
 ﻿Public Class FormVentas
+    Dim hay_articulos_cargados As Boolean = False
+
     Private Sub FormVentas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Soporte.cargar_combo(cmb_tipoDocCLIENTE, Soporte.leerBD_simple("SELECT * FROM tipo_documento"), "id_tipo_documento", "nombre_tipo_documento")
         Soporte.cargar_combo(cmb_producto, Soporte.leerBD_simple("SELECT * FROM productos"), "id_producto", "descripcion")
@@ -9,13 +11,17 @@
     End Sub
 
     Private Sub btn_nuevaVENTA_Click(sender As Object, e As EventArgs) Handles btn_nuevaVENTA.Click
-        Me.habilitar_campos()
+        Me.habilitar_camposVENTA()
+        Me.limpiar_camposVENTA()
+        Me.limpiar_camposCLIENTE()
+        Me.limpiar_camposDETALLE()
+        Me.limpiar_camposFORMAPAGO()
     End Sub
 
     Private Sub limpiar_camposDETALLE()
         Me.cmb_producto.SelectedIndex = -1
         Me.txt_precio.Text = "0,00"
-        Me.txt_cantidad.Text = "0"
+        Me.txt_cantidad.Text = "1"
     End Sub
 
     Private Sub limpiar_camposFORMAPAGO()
@@ -38,7 +44,7 @@
         Me.txt_nombreCLIENTE.Text = ""
     End Sub
 
-    Private Sub habilitar_campos()
+    Private Sub habilitar_camposVENTA()
         'CLIENTE
         Me.cmb_tipoDocCLIENTE.Enabled = True
         Me.txt_nroDocCLIENTE.Enabled = True
@@ -47,5 +53,20 @@
         Me.cmb_producto.Enabled = True
         Me.txt_cantidad.Enabled = True
         Me.btn_agregarDETALLE.Enabled = True
+    End Sub
+
+    Private Sub habilitar_camposPAGO()
+        'FORMA_PAGO
+        Me.cmb_formaPago.Enabled = True
+        Me.txt_montoFORMAPAGO.Enabled = True
+        Me.btn_agregarFORMAPAGO.Enabled = True
+    End Sub
+
+    Private Sub btn_agregarDETALLE_Click(sender As Object, e As EventArgs) Handles btn_agregarDETALLE.Click
+        If Me.hay_articulos_cargados = False Then
+            Me.hay_articulos_cargados = True
+            Me.habilitar_camposPAGO()
+        End If
+
     End Sub
 End Class
