0d9cc07a357aa636879b9777eb3be39c8e07194e
Agustina <aguusnm@gmail.com>
2017-06-20 19:45:22 -0300

    Formato Reportes

--- PAV1_TPI/ESTADISTICA/EstadisticaUsuario/EstadisticaUsuario.Designer.vb
+++ PAV1_TPI/ESTADISTICA/EstadisticaUsuario/EstadisticaUsuario.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class EstadisticaUsuario
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,28 +20,39 @@ Partial Class EstadisticaUsuario
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(EstadisticaUsuario))
+        Me.usuariosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DatosUsuarios = New PAV1_TPI.DatosUsuarios()
         Me.cmb_temporadas = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.cmb_año = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.DatosUsuarios = New PAV1_TPI.DatosUsuarios()
-        Me.usuariosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        CType(Me.DatosUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.usuariosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DatosUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'usuariosBindingSource
+        '
+        Me.usuariosBindingSource.DataMember = "usuarios"
+        Me.usuariosBindingSource.DataSource = Me.DatosUsuarios
+        '
+        'DatosUsuarios
+        '
+        Me.DatosUsuarios.DataSetName = "DatosUsuarios"
+        Me.DatosUsuarios.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'cmb_temporadas
         '
         Me.cmb_temporadas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmb_temporadas.FormattingEnabled = True
         Me.cmb_temporadas.Items.AddRange(New Object() {"Otoño-Invierno", "Primavera-Verano"})
-        Me.cmb_temporadas.Location = New System.Drawing.Point(90, 25)
+        Me.cmb_temporadas.Location = New System.Drawing.Point(93, 61)
         Me.cmb_temporadas.Name = "cmb_temporadas"
         Me.cmb_temporadas.Size = New System.Drawing.Size(121, 25)
         Me.cmb_temporadas.TabIndex = 1
@@ -49,7 +60,7 @@ Partial Class EstadisticaUsuario
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(9, 28)
+        Me.Label1.Location = New System.Drawing.Point(12, 64)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(75, 17)
         Me.Label1.TabIndex = 2
@@ -57,14 +68,17 @@ Partial Class EstadisticaUsuario
         '
         'ReportViewer1
         '
+        Me.ReportViewer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         ReportDataSource1.Name = "DataSet1"
         ReportDataSource1.Value = Me.usuariosBindingSource
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "PAV1_TPI.Usuario.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(12, 67)
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 92)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.ServerReport.BearerToken = Nothing
-        Me.ReportViewer1.Size = New System.Drawing.Size(748, 489)
+        Me.ReportViewer1.Size = New System.Drawing.Size(901, 578)
         Me.ReportViewer1.TabIndex = 3
         '
         'cmb_año
@@ -72,7 +86,7 @@ Partial Class EstadisticaUsuario
         Me.cmb_año.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmb_año.FormattingEnabled = True
         Me.cmb_año.Items.AddRange(New Object() {"2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"})
-        Me.cmb_año.Location = New System.Drawing.Point(275, 25)
+        Me.cmb_año.Location = New System.Drawing.Point(278, 61)
         Me.cmb_año.Name = "cmb_año"
         Me.cmb_año.Size = New System.Drawing.Size(121, 25)
         Me.cmb_año.TabIndex = 1
@@ -80,28 +94,31 @@ Partial Class EstadisticaUsuario
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(238, 28)
+        Me.Label2.Location = New System.Drawing.Point(241, 64)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(31, 17)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Año"
         '
-        'DatosUsuarios
+        'Label8
         '
-        Me.DatosUsuarios.DataSetName = "DatosUsuarios"
-        Me.DatosUsuarios.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'usuariosBindingSource
-        '
-        Me.usuariosBindingSource.DataMember = "usuarios"
-        Me.usuariosBindingSource.DataSource = Me.DatosUsuarios
+        Me.Label8.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(230, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(126, Byte), Integer))
+        Me.Label8.Location = New System.Drawing.Point(0, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(925, 61)
+        Me.Label8.TabIndex = 54
+        Me.Label8.Text = "  Estadísticas Vendedores"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'EstadisticaUsuario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.ClientSize = New System.Drawing.Size(772, 568)
+        Me.ClientSize = New System.Drawing.Size(925, 685)
+        Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -110,12 +127,11 @@ Partial Class EstadisticaUsuario
         Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.MaximizeBox = False
         Me.Name = "EstadisticaUsuario"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Estadística de usuario que más vendió por temporada"
-        CType(Me.DatosUsuarios, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "Estadísticas"
         CType(Me.usuariosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DatosUsuarios, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -127,4 +143,5 @@ Partial Class EstadisticaUsuario
     Friend WithEvents Label2 As Label
     Friend WithEvents usuariosBindingSource As BindingSource
     Friend WithEvents DatosUsuarios As DatosUsuarios
+    Friend WithEvents Label8 As Label
 End Class
